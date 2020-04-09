6420c13e16e64b70e42009fd663d78ba30cc4692
Naoki Matsumoto <naokis.com@gmail.com>
2020-04-09 13:05:39 +0900

    板

---
+++ prototype1/app/src/main/java/com/example/prototype1/ui/main/Table.kt
@@ -0,0 +1,13 @@
+package com.example.prototype1.ui.main
+
+class Table{
+    lateinit var category:String
+    lateinit var title:String
+    constructor(){
+    }
+    constructor(category:String,title:String){
+        this.category=category
+        this.title=title
+    }
+
+}
